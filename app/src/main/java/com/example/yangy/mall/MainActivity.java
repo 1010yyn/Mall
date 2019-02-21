f16083dd3462b5a5fc3787aa7c5945d994aa764c
1010yyn <yangyiling1997@yeah.net>
2019-02-21 16:46:16 +0800

    Categories

--- app/src/main/java/com/example/yangy/mall/MainActivity.java
+++ app/src/main/java/com/example/yangy/mall/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.yangy.mall;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    String TAG = "MYTAG";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        Log.i(TAG, "开屏");
+        setContentView(R.layout.start);
+        //TODO——停留3s
+        Log.i(TAG, "开屏停留3s");
+        //TODO——链接数据库，加载主界面内容
+        Log.i(TAG, "链接数据库");
+        //加载主界面内容
+        try {
+            setContentView(R.layout.activity_main);
+            Log.i(TAG, "主界面");
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
     }
 }
